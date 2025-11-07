660b93ca4cb312e28b38a99b7f0cc6c1342702c4
akashtibil99 <akash.shindhe@tibilsolutions.com>
2025-11-07 23:36:01 +0530

    logo fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -177,7 +177,7 @@ function App() {
         <div className="hero-overlay"></div>
         <div className="hero-content">
           <div className="logo-container" data-testid="logo-container">
-              <img src="/avv-logo.png" alt="avv logo" className="avv-logo" />
+              <img src="public/avv-logo.png" alt="avv logo" className="avv-logo" />
           </div>
           <h1 className="hero-title" data-testid="main-title">
             AVV Aspire Solutions
